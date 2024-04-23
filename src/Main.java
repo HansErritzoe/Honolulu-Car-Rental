d08c98d5f60dd1f0681cfa34abcd692b98dd902d
HansErritzoe <h.erritzoe@hotmail.com>
2024-04-23 14:45:39 +0200

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static ArrayList<RentalContracts> rentalContracts = ... FileHandler.getRentalcontract():
+    public static ArrayList<Car> availableCars = ...
+
+    public static void main(String args){
+
+    }
+
 }
