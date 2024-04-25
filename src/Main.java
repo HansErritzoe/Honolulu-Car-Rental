e48f2b3e9506ecc6b932f0b7f74888fd0a43a9bb
HansErritzoe <h.erritzoe@hotmail.com>
2024-04-25 21:22:32 +0200

    Added WelcomeMessage and sexied up mainMenu

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static Scanner userInput = new Scanner(System.in);
     public static void main(String[] args){
 
-        PrivateCustomer test1 = new PrivateCustomer("Bobby Bobsen","Bobsalley 12", 2500, "Honolulu", 25503425, "bobbyBobsen123@hotmail.com",123456789, LocalDate.now());
+        /* PrivateCustomer test1 = new PrivateCustomer("Bobby Bobsen","Bobsalley 12", 2500, "Honolulu", 25503425, "bobbyBobsen123@hotmail.com",123456789, LocalDate.now());
         Car c1 = new Car("Chevrolet", "Spark", "Diesel", "MGN 530", 200505,250,true,true);
         Car c2 = new Car("Kia", "Rio", "Gasoline 95","YHP 353",2018,2500,false,false);
         FamilyCar c3 = new FamilyCar("Mitsubishi", "Mirage", "Gasoline 90", "SDE 320", 202004,2300,true,true,8);
@@ -21,7 +21,8 @@ public class Main {
         FileHandler.writeCarsToFile(rentalCars);
         for(Car car : rentalCars){
             System.out.println(car.getBrand());
-        }
+        }*/
+        UI.welcomeMessage();
         UI.mainMenu();
     }
 }
--- src/UI.java
+++ src/UI.java
@@ -5,7 +5,17 @@ public class UI {
         try {
             int answer;
             do {
-                System.out.println("Du er i menuen");
+                System.out.println();
+                System.out.println("                   Main Menu                   ");
+                System.out.println("===============================================");
+                System.out.println("      Input a number to select an option       ");
+                System.out.println("===============================================");
+                System.out.println("  type 1: Create Rental Contract/Car/Customer  ");
+                System.out.println("  type 2: Edit Rental Contract/Car/Customer    ");
+                System.out.println("  type 3: Process Rental Contract              ");
+                System.out.println("  type 4: View all Rental Contract/Car/Customer");
+                System.out.println("  type 5: Delete Rental Contract/Car/Customer  ");
+                System.out.println("  type 0: To exit program                      ");
                 answer = Main.userInput.nextInt();
                 switch (answer) {
                     case 1:
@@ -36,5 +46,24 @@ public class UI {
             Main.userInput.nextLine();//clearer scanneren
             mainMenu();
         }
+    }
+
+    public static void welcomeMessage(){
+        try {
+            System.out.println("===============================================");
+            System.out.println("        Welcome to Honolulu Car Rental");
+            System.out.println("===============================================");
+            System.out.println();
+            System.out.println("         -           __");
+            System.out.println("       --          ~( @\\   \\");
+            System.out.println("      ---   _________]_[__/_>________");
+            System.out.println("           /  ____ \\ <>     |  ____  \\");
+            System.out.println("          =\\_/ __ \\_\\_______|_/ __ \\__D");
+            System.out.println("_______________(__)_____________(__)___________");
+            Thread.sleep(2000);
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+
     }
 }
